2352d7e2fd76266ec7afd14738a916919d117c88
Mal-back <purmerinos@protonmail.com>
2024-02-14 16:19:30 +0100

    Refactor find logical operator part)

--- srcs/ast/find_logical_opertor.c
+++ srcs/ast/find_logical_opertor.c
@@ -53,6 +53,28 @@ void	discard_parenthesis(t_token_stream_node **token_stream)
 	return ;
 }
 
+static void	update_stream_if_needed(t_token_stream_node **token_stream)
+{
+	if (*token_stream != NULL)
+	{
+		*token_stream = (*token_stream)->next;
+	}
+}
+
+static t_bool	search_for_relevant_operator(t_token_stream_node *looked_node,
+		t_token_stream_node **next_logical_operator, int mode)
+{
+	t_bool	return_value;
+
+	return_value = FALSE;
+	if (is_searched_operator(looked_node, mode) == TRUE)
+	{
+		*next_logical_operator = looked_node;
+		return_value = TRUE;
+	}
+	return (return_value);
+}
+
 t_token_stream_node	*find_logical_operator(t_token_stream_node *token_stream,
 		int mode)
 {
@@ -62,15 +84,12 @@ t_token_stream_node	*find_logical_operator(t_token_stream_node *token_stream,
 	while (token_stream != NULL)
 	{
 		jump_above_parenthesis_if_needed(&token_stream);
-		if (is_searched_operator(token_stream, mode) == TRUE)
+		if (search_for_relevant_operator(token_stream, &next_logical_operator,
+				mode) == TRUE)
 		{
-			next_logical_operator = token_stream;
 			break ;
 		}
-		if (token_stream != NULL)
-		{
-			token_stream = token_stream->next;
-		}
+		update_stream_if_needed(&token_stream);
 	}
 	return (next_logical_operator);
 }
