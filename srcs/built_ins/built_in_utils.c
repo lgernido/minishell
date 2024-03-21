ccc7fd31720020aa8580ccafa7f79a9b59ff3add
Mal-back <purmerinos@protonmail.com>
2024-03-21 13:26:06 +0100

    Fix the bug with ft_getenv

--- srcs/built_ins/built_in_utils.c
+++ srcs/built_ins/built_in_utils.c
@@ -51,6 +51,32 @@ void	print_export(char **env)
 	}
 }
 
+size_t	get_var_name_len(char	*var)
+{
+	size_t	i;
+
+	i = 0;
+	while (var[i] != '\0' && var[i] != '=')
+	{
+		++i;
+	}
+	return (i);
+}
+
+t_bool	match_var(char *searched_var, char *looked_var)
+{
+	const size_t	len = get_var_name_len(looked_var);
+	t_bool	return_value;
+
+	return_value = FALSE;
+	looked_var[len] = '\0';
+	if (ft_strncmp(searched_var, looked_var, len + 1) == 0)
+	{
+		return_value = TRUE;
+	}
+	return (return_value);
+}
+
 char	*ft_getenv(t_core *core, char *var)
 {
 	int				i;
@@ -59,11 +85,15 @@ char	*ft_getenv(t_core *core, char *var)
 	i = 0;
 	var++;
 	if (!ft_strncmp(var, "?", 2))
+	{
 		return (ft_itoa(core->error_code));
+	}
 	while (i < core->env_size)
 	{
-		if (core->env[i] != NULL && !ft_strncmp(core->env[i], var, len))
+		if (core->env[i] != NULL && match_var(var, core->env[i]) == TRUE)
+		{
 			return (ft_strdup(core->env[i] + 1 + len));
+		}
 		i++;
 	}
 	return (NULL);
